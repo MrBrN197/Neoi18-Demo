8f4ea8959a21c95a0388d546d87c44621bc952fa
Brian Orora <brianorora@gmail.com>
2024-01-30 18:49:40 +0300

    source events

--- client.c
+++ client.c
@@ -7,9 +7,16 @@ typedef struct wl_interface wl_interface;
 typedef struct wl_proxy wl_proxy;
 typedef struct wl_registry wl_registry;
 typedef struct timespec timespec;
+typedef struct wl_callback wl_callback;
 
-#define status(x)                                                              \
-  if (x != 0) {                                                                \
+#define status(X)                                                              \
+  if ((X) != 0) {                                                              \
+    printf("failed\n");                                                        \
+    return 1;                                                                  \
+  };
+
+#define check(X)                                                               \
+  if (!(X)) {                                                                  \
     printf("failed\n");                                                        \
     return 1;                                                                  \
   };
@@ -17,6 +24,7 @@ typedef struct timespec timespec;
 int main() {
   wl_display *server;
   int init = 0;
+  int s;
   timespec ts = {.tv_sec = 1};
 
   while (!init) {
@@ -30,22 +38,31 @@ int main() {
     thrd_sleep(&ts, NULL);
   }
 
-  int s = wl_display_dispatch(server);
-  if (s != 0) {
-    printf("failed to read from queue\n");
-    return 1;
+  wl_registry *registry = wl_display_get_registry(server);
+  if (!registry) {
+    return 3;
+    printf("failed\n");
   }
+
+  // set event listener
+  wl_callback *cb = wl_display_sync(server);
+  status(cb == 0 ? 1 : 0);
+  s = wl_display_flush(server);
+  check(s > 0);
+  printf("display flushed\n");
+  s = wl_display_roundtrip(server);
+  status(s);
+  printf("roundtrip\n");
+
+  // s = wl_display_dispatch(server);
+  // if (s != 0) {
+  //   printf("failed to read from queue\n");
+  //   return 1;
+  // }
+  // printf("read bytes\n");
 
   s = wl_display_get_error(server);
   status(s);
-
-  // wl_registry *registry = wl_display_get_registry(server);
-  // printf("registry:%p\n", registry);
-  //
-  // if (!registry) {
-  //   return 3;
-  //   printf("failed\n");
-  // }
 
   // wl_display_get_fd
   return 0;
--- server.c
+++ server.c
@@ -7,18 +7,17 @@ typedef struct wl_display wl_display;
 
 typedef struct wl_display wl_display;
 typedef struct wl_event_source wl_event_source;
+typedef struct wl_list wl_list;
 
 void logger_func(void *user_data, enum wl_protocol_logger_type direction,
                  const struct wl_protocol_logger_message *message) {
   printf("[LOGGER]\n");
 }
 
-// int get_event(struct wl_event_loop *evloop) {
-//   s = wl_event_loop_get_fd(evloop);
-// }
+void idle_cb_f(void *data) { printf("[Idle]\n"); }
 
-int evsource_cb(void *data) {
-  printf("event_source callback\n");
+int evsource_cb_f(void *data) {
+  printf("[Timer]\n");
   return 0;
 }
 
@@ -29,18 +28,13 @@ int fdsource_cb(int fd, uint32_t mask, void *data) {
 //
 
 int start_server() {
+  int s;
   printf("server: creating display \n");
   wl_display *disp = wl_display_create();
-  if (!disp)
+  if (!disp) {
     printf("wl_display_create => null \n");
-
-  int s;
-  s = wl_display_add_socket(disp, "/run/user/1000/wl-server-0");
-  if (s != 0) {
-    printf("wl_display_add_socket => %d\n", s);
     return 1;
   }
-  printf("server listenieng\n");
 
   void *user_data = (void *)(0);
   wl_protocol_logger *logger = wl_display_add_protocol_logger(
@@ -51,23 +45,54 @@ int start_server() {
     return 2;
   }
 
+  s = wl_display_add_socket(disp, "/run/user/1000/wl-server-0");
+  if (s != 0) {
+    printf("wl_display_add_socket => %d\n", s);
+    return 1;
+  }
+  printf("server listenieng\n");
+
   struct wl_event_loop *ev_loop = wl_event_loop_create();
   if (!ev_loop) {
-    printf("faile\n");
+    printf("failed\n");
     return 3;
   }
 
   wl_event_source *evsource =
-      wl_event_loop_add_timer(ev_loop, &evsource_cb, (void *)0);
+      wl_event_loop_add_timer(ev_loop, &evsource_cb_f, (void *)0);
   if (!evsource) {
     printf("event_loop_add_timer => null\n");
     return 8;
   }
 
-  int timeout = 1000;
-  for (int i = 0; i < 1000; i++) {
+  wl_event_source *idle_source =
+      wl_event_loop_add_idle(ev_loop, &idle_cb_f, (void *)0);
+  if (!idle_source) {
+    printf("Runnig\n");
+    return 8;
+  }
+
+  int timeout = 240;
+  printf("... => %d\n", s);
+  int i = 0;
+  while (true) {
+    i += 1;
+    printf("\r     ");
+    printf("\r%d", i);
     s = wl_event_loop_dispatch(ev_loop, timeout);
-    printf("polling ... event_loop_dispatch => %d\n", s);
+    if (s != 0) {
+      printf("Failed\n");
+      return 1;
+    }
+    fflush(stdout);
+  };
+
+  wl_list *prev = wl_display_get_client_list(disp);
+  wl_list *list = prev->next;
+  printf("list => %p\n", prev);
+  while (list != prev) {
+    printf("count %p\n", list);
+    list = list->next;
   }
 
   printf("wl_event_loop_destroy()\n");
